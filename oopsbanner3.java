aa7f5b4d5caeea25d4060f8e885d61a03918a586
SriViswatejaPenugonda <sriviswatejapenugonda2486@gmail.com>
2026-04-21 13:17:30 +0530

    add oopsbanner3

---
+++ oopsbanner3.java
@@ -0,0 +1,10 @@
+public class oopsbanner3 {
+    public static void main(String[] args) {
+        //uc3
+        System.out.println(String.join(" "," ***** "," ***** "," ******"," ***** "));
+        System.out.println(String.join(" ","*     *","*     *","*     *","*     *"));
+        System.out.println(String.join(" ","*     *","*     *","****** "," ***** "));
+        System.out.println(String.join(" ","*     *","*     *","*      ","      *"));
+        System.out.println(String.join(" "," ***** "," ***** ","*      "," ***** "));
+    }
+}
